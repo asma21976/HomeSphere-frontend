b8d629eac147d7b50560542d3f339aa02994b8df
Asma <asmahashmi94@gmail.com>
2023-10-07 12:54:24 -0600

    Initial Homepage - Draft

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="App-name">
+          <h1>HomeSphere</h1>
+        </div>
+        <div className="App-options">
+          <div className="App-subheader">
+            <p1>Navigating Tomorrow's Housing Landscape, Today</p1>
+          </div>
+          <p1>Choose your map!</p1>
+          <button>Congestion Map</button>
+          <button>House Price Map</button>
+          <button>Accessibility</button>
+          <button>Housing Develeopment Zone</button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
