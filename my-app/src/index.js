9d1f5a4e93d5c3235a0e82426e30cc0cfb3eee15
Girimer Singh <81447338+girimer@users.noreply.github.com>
2024-02-24 09:39:53 -0700

    remove react StrictMode

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,11 +7,13 @@ import reportWebVitals from './reportWebVitals';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // disabling strict mode as there are 2 api get calls made to backened for each map load 
+  //https://stackoverflow.com/questions/73174642/component-making-two-api-calls-from-single-dispatch
+  //<React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  //</React.StrictMode>,
 );
 
 reportWebVitals();
