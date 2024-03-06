c963a1a159aef93fde470dc935732beda8bbf1d1
Haniya Ahmed <haniya.ahmed@ucalgary.ca>
2024-03-06 13:50:44 -0700

    frontend elements added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import CommunityVacancy from "./components/Community_Vacancy_Map.js";
 import Maps from "./components/Maps";
 import Home from "./pages/home.js";
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/maps/community_vacancy" element={<CommunityVacancy />} />
       <Route path="/maps/:mapType" element={<Maps />} />
     </Routes>
   );
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "font-awesome/css/font-awesome.min.css";
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 root.render(
-  // disabling strict mode as there are 2 api get calls made to backened for each map load 
+  // disabling strict mode as there are 2 api get calls made to backened for each map load
   //https://stackoverflow.com/questions/73174642/component-making-two-api-calls-from-single-dispatch
   //<React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
   //</React.StrictMode>,
 );
 
