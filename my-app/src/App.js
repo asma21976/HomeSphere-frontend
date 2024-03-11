828cdf6176beb403c75b10ca1aa52c4991529d4f
Asma <asmahashmi94@gmail.com>
2024-03-11 13:35:04 -0600

    updating fetch

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import CommunityVacancy from "./components/Community_Vacancy_Map.js";
 import Maps from "./components/Maps";
 import Home from "./pages/home.js";
 
@@ -8,7 +7,6 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/maps/community_vacancy" element={<CommunityVacancy />} />
       <Route path="/maps/:mapType" element={<Maps />} />
     </Routes>
   );
--- my-app/src/components/Maps.js
+++ my-app/src/components/Maps.js
@@ -38,62 +38,24 @@ function Maps() {
     return `${value}°C`;
   }
 
-  const communityFeatures = {
-    "Demographic and Socioeconomic Indicators": [
-      "Count of Population in Private Households",
-      "Median Household Income",
-      "Count of Population Considered Low Income",
-      "Count of Private Households",
-      "Count of Owner Households",
-      "Count of Renter Households",
-      "Count of Private Households With Income",
-      "Count of Households with Less Than 30 Pct of Total Income on Shelter",
-      "Count of Households with More Than 30 Pct of Total Income on Shelter",
-      "Median Owner Monthly Shelter Cost",
-      "Median Renter Monthly Shelter Cost",
-    ],
-    "Housing Composition Condition": [
-      "Count of Households That Require Regular Maintenance Or Minor Repairs",
-      "Count of Households That Require Major Repairs",
-      "Count of Suitable Households",
-      "Count of Unsuitable Households",
-    ],
-    "Crime And Disorder": [
-      "Crime Count of Prior Year",
-      "Physical and Social Disorder Count of Prior Year",
-    ],
-  };
-
-  const postalFeatures = {
-    PropertyAssessments: ["MedianAssessedValue", "MedianLandSize"],
-    Schools: [
-      "NearestElementarySchool",
-      "NearestJuniorHighSchool",
-      "NearestSeniorHighSchool",
-      "SchoolCountWithin1KM",
-    ],
-    CommunityServices: [
-      "NearestCommunityCentre",
-      "NearestAttraction",
-      "NearestVisitorInformationCentre",
-      "NearestSocialDevelopmentCentre",
-      "ServicesCountWithin1KM",
-    ],
-    Amenities: [
-      "NearestHospital",
-      "NearestLibrary",
-      "NearestPHSClinic",
-      "NearestCourt",
-    ],
-  };
-
   useEffect(() => {
-    fetch(`http://127.0.0.1:8000/api/${mapType}`)
-      .then((data) => data.json())
-      .then((data) => {
-        setMapData(JSON.parse(data));
+    fetch(`http://localhost:3000//api/maps/${mapType}`, {
+      headers: {
+        'AccessToken': 'Kvwf<IQ5qV]nlPooW@'
+      }
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
       })
-      .catch((err) => console.error("Err:", err));
+      .then((data) => {
+        setMapData(data);
+      })
+      .catch((err) => {
+        console.error("Error fetching data:", err);
+      });
   }, [mapType]);
 
   console.log(mapData);
@@ -124,76 +86,9 @@ function Maps() {
                 className="fa-svg-icon"
               />
               <p>Population</p>
-              {/* Congestion */}
             </button>
           </Link>
-          <Link to="/maps/community_vacancy">
-            <button
-              id="vacancy-button"
-              className={`menu-button ${
-                isSelected("community_vacancy")
-                  ? "selected"
-                  : "map-feature-button"
-              }`}
-            >
-              {/* Community  */}
-              <FontAwesomeIcon
-                icon={faMapMarkerAlt}
-                title="Land Vacancy"
-                className="fa-svg-icon"
-              />
-              <p>Vacancy</p>
-            </button>
-          </Link>
-          <Link to="/maps/housing_development_zone">
-            <button
-              id="permits-button"
-              className={`menu-button ${
-                isSelected("housing_development_zone")
-                  ? "selected"
-                  : "map-feature-button"
-              }`}
-            >
-              {/* Urban  */}
-              <FontAwesomeIcon
-                icon={faScroll}
-                title="Building Permits"
-                className="fa-svg-icon"
-              />
-              <p>Permits</p>
-            </button>
-          </Link>
-          <Link to="/maps/house_price_map">
-            <button
-              id="pricing-button"
-              className={`menu-button ${
-                isSelected("house_price_map")
-                  ? "selected"
-                  : "map-feature-button"
-              }`}
-            >
-              <FontAwesomeIcon
-                icon={faDollarSign}
-                title="House Prices"
-                className="fa-svg-icon"
-              />
-              <p>House Prices</p>
-            </button>
-          </Link>
-          <button
-            id="machine-learning-button"
-            className={`menu-button ${
-              isSelected("house_price_map") ? "selected" : "map-feature-button"
-            }`}
-            onClick={toggleMLWindow}
-          >
-            <FontAwesomeIcon
-              icon={faSquarePollVertical}
-              title="Machine Learning Housing Analaysis"
-              className="fa-svg-icon"
-            />
-            <p>Housing Analysis</p>
-          </button>
+          {/* Other menu buttons */}
         </div>
       </div>
       <div id="mapContainer">
@@ -212,67 +107,6 @@ function Maps() {
         className={`${showMLWindow ? "machine-learning-window " : "hidden"}`}
       >
         {/* Machine learning window content */}
-        <button type="button" class="cancelML" onClick={toggleMLWindow}>
-          X
-        </button>
-        <p className="disclaimer">
-          Disclaimer: Please not that our housing analysis system focuses
-          exclusively on the northeast scope of Calgary and offers a broad
-          estimation of housing and community analytics based on data sources
-          from the City of Calgary. This information must not be regarded as
-          definitive and should only serve as a guide and support for developers
-          and residents.
-        </p>
-        <h2>Select Analysis Criteria</h2>
-        <div
-          id="features"
-          className="features"
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-around",
-            width: "97%",
-          }}
-        >
-          {Object.entries(communityFeatures).map(([key, values], index) => (
-            <div
-              key={index}
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "flex-start",
-              }}
-            >
-              <h3>{key}</h3>
-              {values.map((value, valueIndex) => (
-                <div key={valueIndex} style={{ margin: "5px 0" }}>
-                  <input
-                    type="checkbox"
-                    id={`${key}_${valueIndex}`}
-                    name={value}
-                  />
-                  <label htmlFor={`${key}_${valueIndex}`}>{value}</label>
-                </div>
-              ))}
-            </div>
-          ))}
-        </div>
-        <div id="slider" className="slider">
-          <Box sx={{ width: 200 }}>
-            <Slider
-              defaultValue={3}
-              getAriaValueText={valuetext}
-              valueLabelDisplay="auto"
-              shiftStep={1}
-              step={1}
-              min={1}
-              max={30}
-              className="slider-component"
-            />
-          </Box>
-        </div>
-
-        <button type="submit">Run</button>
       </div>
     </div>
   );
