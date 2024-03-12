6742e14b9e12366a9f2f831d378d03404d9e2fa7
Haniya Ahmed <haniya.ahmed@ucalgary.ca>
2024-03-12 15:03:22 -0600

    new home page design implemented

--- my-app/src/pages/home.js
+++ my-app/src/pages/home.js
@@ -9,30 +9,15 @@ function Home() {
         <div className="App-name">
           <h1>HomeSphere</h1>
         </div>
-        <div className="App-options">
-          <div className="App-subheader">
-            <p>Navigating Tomorrow's Housing Landscape, Today</p>
-          </div>
-          <div className="App-paragraph">
-            <p>Choose your map!</p>
-          </div>
-          <Link to="/maps/community_vacancy">
-            <button>Community Vacancy</button>
-          </Link>
+      </header>
+      <body>
+        <div className="center-content">
+          <h1 className="slogan"> Shaping Calgary's Future: </h1>
           <Link to="/maps/congestion_map">
-            <button>Congestion Map</button>
-          </Link>
-          <Link to="/maps/house_price_map">
-            <button>House Price Map</button>
-          </Link>
-          <Link to="/maps/accessibility">
-            <button>Accessibility</button>
-          </Link>
-          <Link to="/maps/housing_development_zone">
-            <button>Housing Development Zone</button>
+            <button className="get-started">Get Started</button>
           </Link>
         </div>
-      </header>
+      </body>
     </div>
   );
 }
