acc99c99a0706d962c1fdd1c73fde65cdbff890a
Asma <asmahashmi94@gmail.com>
2024-03-20 12:48:27 -0600

    Sidebar modifications

--- my-app/src/components/Maps.js
+++ my-app/src/components/Maps.js
@@ -20,6 +20,7 @@ function Maps() {
   const location = useLocation();
   const [showMLWindow, setShowMLWindow] = useState(false);
   const [collapsed, setCollapsed] = useState(false);
+  const [lastButtonExpanded, setLastButtonExpanded] = useState(false);
   const [mapData, setMapData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -35,6 +36,10 @@ function Maps() {
 
   const toggleMLWindow = () => {
     setShowMLWindow(!showMLWindow);
+  };
+
+  const handleLastButtonToggle = () => {
+    setLastButtonExpanded(!lastButtonExpanded);
   };
 
   function valuetext(value) {
@@ -131,95 +136,96 @@ function Maps() {
           HOMESPHERE
         </h1>
       </div>
-      <div id="sidebar" className={`sidebar ${collapsed ? "short" : ""}`}>
-        <div className="HomeSphere-Title">
-          <h1>HOMESPHERE</h1>
-        </div>
-        <div className="close-sidebar-btn" onClick={handleToggleSidebar}>
-          <button type="button">X</button>
-        </div>
-        <div id="buttons-container">
-          <Link to="/maps/congestion">
-            <button
-              id="population-button"
-              className={`menu-button ${
-                isSelected("congestion") ? "selected" : "map-feature-button"
-              }`}
-            >
-              <FontAwesomeIcon
-                icon={faArrowUpRightDots}
-                title="Community Population"
-                className="fa-svg-icon"
-              />
-              {/* Congestion */}
-            </button>
-          </Link>
-          <Link to="/maps/vacancy_per_community">
-            <button
-              id="vacancy-button"
-              className={`menu-button ${
-                isSelected("vacancy_per_community")
-                  ? "selected"
-                  : "map-feature-button"
-              }`}
-            >
-              {/* Community  */}
-              <FontAwesomeIcon
-                icon={faMapMarkerAlt}
-                title="Land Vacancy"
-                className="fa-svg-icon"
-              />
-            </button>
-          </Link>
-          <Link to="/maps/housing_development_zone">
-            <button
-              id="permits-button"
-              className={`menu-button ${
-                isSelected("housing_development_zone")
-                  ? "selected"
-                  : "map-feature-button"
-              }`}
-            >
-              {/* Urban  */}
-              <FontAwesomeIcon
-                icon={faScroll}
-                title="Building Permits"
-                className="fa-svg-icon"
-              />
-            </button>
-          </Link>
-          <Link to="/maps/property_value_per_community">
-            <button
-              id="pricing-button"
-              className={`menu-button ${
-                isSelected("property_value_per_community")
-                  ? "selected"
-                  : "map-feature-button"
-              }`}
-            >
-              <FontAwesomeIcon
-                icon={faDollarSign}
-                title="House Prices"
-                className="fa-svg-icon"
-              />
-            </button>
-          </Link>
+
+    <div id="sidebar" className={`sidebar ${collapsed && !lastButtonExpanded ? "short" : "expanded"}`}>
+      <div className="HomeSphere-Title">
+        <h1>HOMESPHERE</h1>
+      </div>
+      {/* <div className="close-sidebar-btn" onClick={handleToggleSidebar}>
+        <button type="button">X</button>
+      </div> */}
+      <div id="buttons-container">
+        <Link to="/maps/congestion">
           <button
-            id="machine-learning-button"
+            id="population-button"
             className={`menu-button ${
-              isSelected("house_price_map") ? "selected" : "map-feature-button"
+              isSelected("congestion") ? "selected" : "map-feature-button"
             }`}
-            onClick={toggleMLWindow}
           >
-            <FontAwesomeIcon
-              icon={faSquarePollVertical}
-              title="Machine Learning Housing Analaysis"
+            <span className="button-name"> Community <br/> Population</span>
+              <FontAwesomeIcon
+              icon={faArrowUpRightDots}
+              title="Community Population"
               className="fa-svg-icon"
             />
           </button>
-        </div>
-        <div
-          id="machine-learning-window"
+        </Link>
+        <Link to="/maps/vacancy_per_community">
+          <button
+            id="vacancy-button"
+            className={`menu-button ${
+              isSelected("vacancy_per_community") ? "selected" : "map-feature-button"
+            }`}
+          >
+            <span className="button-name"> Community<br/>Vacancy</span>
+              <FontAwesomeIcon
+              icon={faMapMarkerAlt}
+              title="Land Vacancy"
+              className="fa-svg-icon"
+            />
+          </button>
+        </Link>
+        <Link to="/maps/housing_development_zone">
+          <button
+            id="permits-button"
+            className={`menu-button ${
+              isSelected("housing_development_zone") ? "selected" : "map-feature-button"
+            }`}
+          >
+            <span className="button-name"> Housing<br/>Development</span>
+              <FontAwesomeIcon
+              icon={faScroll}
+              title="Building Permits"
+              className="fa-svg-icon"
+            />
+          </button>
+        </Link>
+        <Link to="/maps/property_value_per_community">
+          <button
+            id="pricing-button"
+            className={`menu-button ${
+              isSelected("property_value_per_community") ? "selected" : "map-feature-button"
+            }`}
+          >
+          <span className="button-name"> House<br/>Prices</span>
+              <FontAwesomeIcon
+              icon={faDollarSign}
+              title="House Prices"
+              className="fa-svg-icon"
+            />
+          </button>
+        </Link>
+        <button
+          id="machine-learning-button"
+          className={`menu-button ${
+            isSelected("house_price_map") ? "selected" : "map-feature-button"
+          }`}
+          onClick={() => {
+            toggleMLWindow();
+            handleLastButtonToggle();
+          }}
+        >
+        <span className="button-name"> ML<br/>House Analysis</span>
+            <FontAwesomeIcon
+            icon={faSquarePollVertical}
+            title="Machine Learning Housing Analysis"
+            className="fa-svg-icon"
+          />
+        </button>
+      </div>
+    <div
+
+      id="machine-learning-window"
           className={`${showMLWindow ? "machine-learning-window " : "hidden"}`}
         >
           {Object.entries(communityFeatures).map(([key, values], index) => (
