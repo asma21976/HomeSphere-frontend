745236d8402dd5857c4d4962e6ee69a5604b7a01
Asma <asmahashmi94@gmail.com>
2024-03-21 11:48:03 -0600

    Fixing Community level and postal level

--- my-app/src/components/Maps.js
+++ my-app/src/components/Maps.js
@@ -49,11 +49,11 @@ function Maps() {
   }
 
   const handleMLOptionChange = (event) => {
-    console.log("New ML option selected:", event.target.value); // Debugging
-
+    console.log("New ML option selected:", event.target.value);
+    
     setSelectedMLOption(event.target.value);
   };
-
+  
   function getFeatures() {
     const features =
       selectedMLOption === "community-level"
@@ -188,16 +188,6 @@ function Maps() {
 
   return (
     <div>
-      {/* <div id="homeBar" className="homeBar">
-        <h1>
-          <FontAwesomeIcon
-            icon={faBars}
-            className="hamburger"
-            onClick={handleToggleSidebar}
-          />
-          HOMESPHERE
-        </h1>
-      </div> */}
       <div
         id="sidebar"
         className={`sidebar ${!lastButtonExpanded ? "shortened" : ""} ${
@@ -207,89 +197,86 @@ function Maps() {
         <div className="HomeSphere-Title">
           <h1>HOMESPHERE</h1>
         </div>
-        {/* <div className="close-sidebar-btn" onClick={handleToggleSidebar}>
-        <button type="button">X</button>
-      </div> */}
-        <div id="buttons-container">
-          <Link to="/maps/congestion">
+          <div id="buttons-container">
+            <Link to="/maps/congestion">
+              <button
+                id="population-button"
+                className={`menu-button ${
+                  isSelected("congestion") ? "selected" : "map-feature-button"
+                }`}
+              >
+                <FontAwesomeIcon
+                  icon={faArrowUpRightDots}
+                  title="Community Population"
+                  className="fa-svg-icon"
+                />
+              </button>
+            </Link>
+            <Link to="/maps/vacancy_per_community">
+              <button
+                id="vacancy-button"
+                className={`menu-button ${
+                  isSelected("vacancy_per_community")
+                    ? "selected"
+                    : "map-feature-button"
+                }`}
+              >
+                <FontAwesomeIcon
+                  icon={faMapMarkerAlt}
+                  title="Land Vacancy"
+                  className="fa-svg-icon"
+                />
+              </button>
+            </Link>
+            <Link to="/maps/housing_development_zone">
+              <button
+                id="permits-button"
+                className={`menu-button ${
+                  isSelected("housing_development_zone")
+                    ? "selected"
+                    : "map-feature-button"
+                }`}
+              >
+                <FontAwesomeIcon
+                  icon={faScroll}
+                  title="Building Permits"
+                  className="fa-svg-icon"
+                />
+              </button>
+            </Link>
+            <Link to="/maps/property_value_per_community">
+              <button
+                id="pricing-button"
+                className={`menu-button ${
+                  isSelected("property_value_per_community")
+                    ? "selected"
+                    : "map-feature-button"
+                }`}
+              >
+                <FontAwesomeIcon
+                  icon={faDollarSign}
+                  title="House Prices"
+                  className="fa-svg-icon"
+                />
+              </button>
+            </Link>
             <button
-              id="population-button"
+              id="machine-learning-button"
               className={`menu-button ${
-                isSelected("congestion") ? "selected" : "map-feature-button"
+                isSelected("house_price_map") ? "selected" : "map-feature-button"
               }`}
+              onClick={() => {
+                toggleMLWindow();
+                handleLastButtonToggle();
+              }}
             >
               <FontAwesomeIcon
-                icon={faArrowUpRightDots}
-                title="Community Population"
+                icon={faSquarePollVertical}
+                title="Machine Learning Housing Analysis"
                 className="fa-svg-icon"
               />
             </button>
-          </Link>
-          <Link to="/maps/vacancy_per_community">
-            <button
-              id="vacancy-button"
-              className={`menu-button ${
-                isSelected("vacancy_per_community")
-                  ? "selected"
-                  : "map-feature-button"
-              }`}
-            >
-              <FontAwesomeIcon
-                icon={faMapMarkerAlt}
-                title="Land Vacancy"
-                className="fa-svg-icon"
-              />
-            </button>
-          </Link>
-          <Link to="/maps/housing_development_zone">
-            <button
-              id="permits-button"
-              className={`menu-button ${
-                isSelected("housing_development_zone")
-                  ? "selected"
-                  : "map-feature-button"
-              }`}
-            >
-              <FontAwesomeIcon
-                icon={faScroll}
-                title="Building Permits"
-                className="fa-svg-icon"
-              />
-            </button>
-          </Link>
-          <Link to="/maps/property_value_per_community">
-            <button
-              id="pricing-button"
-              className={`menu-button ${
-                isSelected("property_value_per_community")
-                  ? "selected"
-                  : "map-feature-button"
-              }`}
-            >
-              <FontAwesomeIcon
-                icon={faDollarSign}
-                title="House Prices"
-                className="fa-svg-icon"
-              />
-            </button>
-          </Link>
-          <button
-            id="machine-learning-button"
-            className={`menu-button ${
-              isSelected("house_price_map") ? "selected" : "map-feature-button"
-            }`}
-            onClick={() => {
-              toggleMLWindow();
-              handleLastButtonToggle();
-            }}
-          >
-            <FontAwesomeIcon
-              icon={faSquarePollVertical}
-              title="Machine Learning Housing Analysis"
-              className="fa-svg-icon"
-            />
-          </button>
-        </div>
+          </div>
         <div
           id="machine-learning-window"
           className={`${showMLWindow ? "machine-learning-window " : "hidden"}`}
@@ -300,11 +287,11 @@ function Maps() {
                 type="radio"
                 id="community-level-option"
                 name="ml-options"
-                value="communnity-level"
+                value="community-level"
                 checked={selectedMLOption === "community-level"}
                 onChange={handleMLOptionChange}
               />
-              <label for="community-level-option">Community Level</label>
+              <label htmlFor="community-level-option">Community Level</label>
             </p>
 
             <p>
@@ -316,9 +303,10 @@ function Maps() {
                 checked={selectedMLOption === "postal-code-level"}
                 onChange={handleMLOptionChange}
               />
-              <label for="postal-code-level-option">Sub-Community Level</label>
-            </p>
-          </form>
+              <label htmlFor="postal-code-level-option">Sub-Community Level</label>
+          </p>
+        </form>
+
           <div id="slider" className="slider">
             <h3>Number of categories:</h3>
             <Box sx={{ width: 200 }}>
@@ -380,18 +368,3 @@ function Maps() {
 }
 
 export default Maps;
-
-/*
-<Box sx={{ width: 200 }}>
-            <Slider
-              defaultValue={3}
-              getAriaValueText={valuetext}
-              valueLabelDisplay="auto"
-              shiftStep={1}
-              step={1}
-              min={1}
-              max={30}
-              className="slider-component"
-            />
-          </Box>
-*/
