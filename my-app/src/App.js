daf878dfe950a1cfb8aeabf947f717a6a14fd4b4
Haniya Ahmed <haniya.ahmed@ucalgary.ca>
2024-03-21 14:06:06 -0600

    new changes with attempt to make ML work

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Maps from './components/Maps';
-import Home from './pages/home.js';
+import Maps from "./components/Maps";
+import Home from "./pages/home.js";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/maps/:mapType" element={<Maps />} />
+      <Route path="/maps/:mL" element={<Maps />} />
     </Routes>
   );
 }
--- my-app/src/components/Maps.js
+++ my-app/src/components/Maps.js
@@ -50,57 +50,141 @@ function Maps() {
 
   const handleMLOptionChange = (event) => {
     console.log("New ML option selected:", event.target.value);
-    
+
     setSelectedMLOption(event.target.value);
   };
-  
+
+  const isChecked = (event) => {
+    const features =
+      selectedMLOption === "community-level"
+        ? communityFeatures
+        : postalFeatures;
+    const isChecked = event.target.checked;
+    const key = event.target.id;
+    if (isChecked) {
+      features[key] = true;
+    } else if (!isChecked) {
+      features[key] = false;
+    }
+
+    console.log(features);
+    console.log(key);
+  };
+
+  const clusterCount = (event) => {
+    const features =
+      selectedMLOption === "community-level"
+        ? communityFeatures
+        : postalFeatures;
+    const numClusters = event.target.value;
+    features.n_clusters = numClusters;
+
+    console.log(features);
+    console.log(numClusters);
+  };
+
   function getFeatures() {
     const features =
       selectedMLOption === "community-level"
         ? communityFeatures
         : postalFeatures;
-    return Object.entries(features).map(([key, values], index) => (
-      <div key={index}>
-        <h3>{key}</h3>
-        {values.map((value, valueIndex) => (
-          <div
-            className="machine-learning-window_boxes"
-            key={valueIndex}
-            style={{ margin: "5px 0" }}
-          >
-            <input type="checkbox" id={`${key}_${valueIndex}`} name={value} />
-            <label htmlFor={`${key}_${valueIndex}`}>{value}</label>
-          </div>
-        ))}
-      </div>
-    ));
+    return Object.entries(features)
+      .slice(0, -2)
+      .map(([feature]) => (
+        <div key={feature} style={{ margin: 5 }}>
+          <input type="checkbox" id={feature} onChange={isChecked} />
+          <label htmlFor={feature}>
+            {feature
+              .replace(/_/g, " ")
+              .replace(/\b\w/g, (l) => l.toUpperCase())}
+          </label>
+        </div>
+      ));
+  }
+
+  function runML() {
+    const mlType =
+      selectedMLOption === "community-level"
+        ? "kmeans_community"
+        : "kmeans_postal";
+
+    const features =
+      selectedMLOption === "community-level"
+        ? communityFeatures
+        : postalFeatures;
+    fetch(`https://home-sphere.ca/api/api/${mlType}`, {
+      method: "POST",
+      headers: {
+        AccessToken: "Kvwf<IQ5qV]nlPooW@",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(features),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok at all");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setMapData(data);
+      })
+      .catch((err) => {
+        console.error("Error fetching data:", err);
+        setError(err.message);
+        setMapData(null);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   }
 
   const communityFeatures = {
-    "Demographic and Socioeconomic Indicators": [
-      "Count of Population in Private Households",
-      "Median Household Income",
-      "Count of Population Considered Low Income",
-      "Count of Private Households",
-      "Count of Owner Households",
-      "Count of Renter Households",
-      "Count of Private Households With Income",
-      "Count of Households with Less Than 30 Pct of Total Income on Shelter",
-      "Count of Households with More Than 30 Pct of Total Income on Shelter",
-      "Median Owner Monthly Shelter Cost",
-      "Median Renter Monthly Shelter Cost",
-    ],
-    "Housing Composition Condition": [
-      "Count of Households That Require Regular Maintenance Or Minor Repairs",
-      "Count of Households That Require Major Repairs",
-      "Count of Suitable Households",
-      "Count of Unsuitable Households",
-    ],
-    "Crime And Disorder": [
-      "Crime Count of Prior Year",
-      "Physical and Social Disorder Count of Prior Year",
-    ],
+    count_of_population_in_private_households: false,
+    median_household_income: false,
+    count_of_population_considered_low_income: false,
+    count_of_private_households: false,
+    count_of_owner_households: false,
+    count_of_renter_households: false,
+    count_of_private_households_with_income: false,
+    count_of_households_with_lt_30_pct_of_total_income_on_shelter: false,
+    count_of_households_with_gt_30_pct_of_total_income_on_shelter: false,
+    median_owner_monthly_shelter_cost: false,
+    median_renter_monthly_shelter_cost: false,
+    count_of_households_that_require_maintenance: false,
+    count_of_households_that_require_major_repairs: false,
+    count_of_suitable_households: false,
+    count_of_unsuitable_households: false,
+    community_crime_count: false,
+    community_disorder_count: false,
+    n_clusters: 3,
+    random_state: 42,
   };
+  // {
+  //   "Demographic and Socioeconomic Indicators": [
+  //     "Count of Population in Private Households",
+  //     "Median Household Income",
+  //     "Count of Population Considered Low Income",
+  //     "Count of Private Households",
+  //     "Count of Owner Households",
+  //     "Count of Renter Households",
+  //     "Count of Private Households With Income",
+  //     "Count of Households with Less Than 30 Pct of Total Income on Shelter",
+  //     "Count of Households with More Than 30 Pct of Total Income on Shelter",
+  //     "Median Owner Monthly Shelter Cost",
+  //     "Median Renter Monthly Shelter Cost",
+  //   ],
+  //   "Housing Composition Condition": [
+  //     "Count of Households That Require Regular Maintenance Or Minor Repairs",
+  //     "Count of Households That Require Major Repairs",
+  //     "Count of Suitable Households",
+  //     "Count of Unsuitable Households",
+  //   ],
+  //   "Crime And Disorder": [
+  //     "Crime Count of Prior Year",
+  //     "Physical and Social Disorder Count of Prior Year",
+  //   ],
+  // };
 
   const postalFeatures = {
     "Property Assessments": ["Median Assessed Value", "Median Land Size"],
@@ -108,7 +192,7 @@ function Maps() {
       "Nearest Elementary School",
       "Nearest Junior-Highschool",
       "Nearest Senior-High School",
-      "School Coun tWithin 1KM",
+      "School Count Within 1KM",
     ],
     "Community Services": [
       "Nearest Community Centre",
@@ -197,86 +281,86 @@ function Maps() {
         <div className="HomeSphere-Title">
           <h1>HOMESPHERE</h1>
         </div>
-          <div id="buttons-container">
-            <Link to="/maps/congestion">
-              <button
-                id="population-button"
-                className={`menu-button ${
-                  isSelected("congestion") ? "selected" : "map-feature-button"
-                }`}
-              >
-                <FontAwesomeIcon
-                  icon={faArrowUpRightDots}
-                  title="Community Population"
-                  className="fa-svg-icon"
-                />
-              </button>
-            </Link>
-            <Link to="/maps/vacancy_per_community">
-              <button
-                id="vacancy-button"
-                className={`menu-button ${
-                  isSelected("vacancy_per_community")
-                    ? "selected"
-                    : "map-feature-button"
-                }`}
-              >
-                <FontAwesomeIcon
-                  icon={faMapMarkerAlt}
-                  title="Land Vacancy"
-                  className="fa-svg-icon"
-                />
-              </button>
-            </Link>
-            <Link to="/maps/housing_development_zone">
-              <button
-                id="permits-button"
-                className={`menu-button ${
-                  isSelected("housing_development_zone")
-                    ? "selected"
-                    : "map-feature-button"
-                }`}
-              >
-                <FontAwesomeIcon
-                  icon={faScroll}
-                  title="Building Permits"
-                  className="fa-svg-icon"
-                />
-              </button>
-            </Link>
-            <Link to="/maps/property_value_per_community">
-              <button
-                id="pricing-button"
-                className={`menu-button ${
-                  isSelected("property_value_per_community")
-                    ? "selected"
-                    : "map-feature-button"
-                }`}
-              >
-                <FontAwesomeIcon
-                  icon={faDollarSign}
-                  title="House Prices"
-                  className="fa-svg-icon"
-                />
-              </button>
-            </Link>
+        <div id="buttons-container">
+          <Link to="/maps/congestion">
             <button
-              id="machine-learning-button"
+              id="population-button"
               className={`menu-button ${
-                isSelected("house_price_map") ? "selected" : "map-feature-button"
+                isSelected("congestion") ? "selected" : "map-feature-button"
               }`}
-              onClick={() => {
-                toggleMLWindow();
-                handleLastButtonToggle();
-              }}
             >
               <FontAwesomeIcon
-                icon={faSquarePollVertical}
-                title="Machine Learning Housing Analysis"
+                icon={faArrowUpRightDots}
+                title="Community Population"
                 className="fa-svg-icon"
               />
             </button>
-          </div>
+          </Link>
+          <Link to="/maps/vacancy_per_community">
+            <button
+              id="vacancy-button"
+              className={`menu-button ${
+                isSelected("vacancy_per_community")
+                  ? "selected"
+                  : "map-feature-button"
+              }`}
+            >
+              <FontAwesomeIcon
+                icon={faMapMarkerAlt}
+                title="Land Vacancy"
+                className="fa-svg-icon"
+              />
+            </button>
+          </Link>
+          <Link to="/maps/housing_development_zone">
+            <button
+              id="permits-button"
+              className={`menu-button ${
+                isSelected("housing_development_zone")
+                  ? "selected"
+                  : "map-feature-button"
+              }`}
+            >
+              <FontAwesomeIcon
+                icon={faScroll}
+                title="Building Permits"
+                className="fa-svg-icon"
+              />
+            </button>
+          </Link>
+          <Link to="/maps/property_value_per_community">
+            <button
+              id="pricing-button"
+              className={`menu-button ${
+                isSelected("property_value_per_community")
+                  ? "selected"
+                  : "map-feature-button"
+              }`}
+            >
+              <FontAwesomeIcon
+                icon={faDollarSign}
+                title="House Prices"
+                className="fa-svg-icon"
+              />
+            </button>
+          </Link>
+          <button
+            id="machine-learning-button"
+            className={`menu-button ${
+              isSelected("house_price_map") ? "selected" : "map-feature-button"
+            }`}
+            onClick={() => {
+              toggleMLWindow();
+              handleLastButtonToggle();
+            }}
+          >
+            <FontAwesomeIcon
+              icon={faSquarePollVertical}
+              title="Machine Learning Housing Analysis"
+              className="fa-svg-icon"
+            />
+          </button>
+        </div>
         <div
           id="machine-learning-window"
           className={`${showMLWindow ? "machine-learning-window " : "hidden"}`}
@@ -303,9 +387,11 @@ function Maps() {
                 checked={selectedMLOption === "postal-code-level"}
                 onChange={handleMLOptionChange}
               />
-              <label htmlFor="postal-code-level-option">Sub-Community Level</label>
-          </p>
-        </form>
+              <label htmlFor="postal-code-level-option">
+                Sub-Community Level
+              </label>
+            </p>
+          </form>
 
           <div id="slider" className="slider">
             <h3>Number of categories:</h3>
@@ -319,12 +405,13 @@ function Maps() {
                 min={1}
                 max={30}
                 className="slider-component"
+                onChange={clusterCount}
               />
             </Box>
           </div>
           <div>{getFeatures()}</div>
           <div id="ml-run-btn" className="ml-run-btn">
-            <button> Run</button>
+            <button onClick={runML}> Run</button>
           </div>
         </div>
       </div>
