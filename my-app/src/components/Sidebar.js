ff696fa3cf5740883c41d78f3f3672c131e4de83
Haniya Ahmed <haniya.ahmed@ucalgary.ca>
2024-03-22 14:07:07 -0600

    sidebar separated

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,4 +1,3 @@
-import Plot from "react-plotly.js";
 import React, { useState, useEffect } from "react";
 import { Link, useParams, useLocation } from "react-router-dom";
 import "../components/styles/Maps.css";
@@ -15,11 +14,8 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import Slider from "@mui/material/Slider";
 import Box from "@mui/material/Box";
-import mapboxgl from "mapbox-gl";
-import "mapbox-gl/dist/mapbox-gl.css";
-import Sidebar from "./Sidebar.js";
 
-function Maps() {
+const Sidebar = () => {
   const { mapType } = useParams();
   const location = useLocation();
   const [showMLWindow, setShowMLWindow] = useState(false);
@@ -30,7 +26,6 @@ function Maps() {
   const [error, setError] = useState(null);
   const [collapseLeft, setCollapseLeft] = useState(false);
   const [selectedMLOption, setSelectedMLOption] = useState("community-level");
-
   const isSelected = (pathSegment) => {
     const currentPath = location.pathname.split("/").pop();
     return currentPath === pathSegment;
@@ -135,10 +130,9 @@ function Maps() {
         ? communityFeatures
         : postalFeatures;
 
-    // This variable is now scoped to runML and can be accessed within nested functions.
     let mapData = null;
 
-    fetch(`https://home-sphere.ca/api/api/${mlType}_info`, {
+    fetch(`https://home-sphere.ca/api/api/${mlType}`, {
       method: "POST",
       headers: {
         AccessToken: "Kvwf<IQ5qV]nlPooW@",
@@ -153,6 +147,66 @@ function Maps() {
         return response.json();
       })
       .then((data) => {
+        fetch(`https://home-sphere.ca/api/api/${mlType}_info`, {
+          method: "POST",
+          headers: {
+            AccessToken: "Kvwf<IQ5qV]nlPooW@",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(features),
+        })
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error("Network response was not ok");
+            }
+            return response.json();
+          })
+          .then((mlTypeInfo) => {
+            console.log(mlTypeInfo);
+
+            printResults(mlTypeInfo, mlType);
+          })
+          .catch((err) => {
+            console.error("Error fetching mlType_info:", err);
+          });
+
+        data.layout = {
+          ...data.layout,
+          margin: { l: 0, r: 0, t: 0, b: 0 },
+          title: "",
+          coloraxis: {
+            ...data.layout.coloraxis,
+            colorbar: {
+              ...data.layout.coloraxis.colorbar,
+              xanchor: "right",
+              yanchor: "middle",
+
+              thickness: 10,
+              x: 0.99,
+              y: 0.5,
+              len: 0.8,
+              width: 0.1,
+              title: {
+                ...data.layout.coloraxis.colorbar.title,
+                side: "right",
+                font: {
+                  family: "Arial Black",
+                  size: 14,
+                },
+              },
+            },
+            colorscale: "Rainbow",
+          },
+          mapbox: {
+            ...data.layout.mapbox,
+            zoom: 11,
+            center: {
+              lat: 51.115,
+              lon: -113.954,
+            },
+          },
+        };
+        setMapData(data);
         console.log(data);
       })
       .catch((err) => {
@@ -229,17 +283,14 @@ function Maps() {
     //   });
   }
 
-  function printResults() {
-    console.log("Selected ML Option:", selectedMLOption);
-    console.log("Map Data:", mapData);
-
-    const jsonData = JSON.stringify(mapData, null, 2);
-
-    const blob = new Blob([jsonData], { type: "application/json" });
+  function printResults(mlTypeInfo, mlType) {
+    const blob = new Blob([JSON.stringify(mlTypeInfo, null, 2)], {
+      type: "application/json",
+    });
 
     const anchor = document.createElement("a");
 
-    anchor.download = "mapData.json";
+    anchor.download = `${mlType}_info.json`;
 
     anchor.href = window.URL.createObjectURL(blob);
 
@@ -247,7 +298,6 @@ function Maps() {
 
     window.URL.revokeObjectURL(anchor.href);
   }
-
   const communityFeatures = {
     count_of_population_in_private_households: false,
     median_household_income: false,
@@ -331,96 +381,172 @@ function Maps() {
     ],
   };
 
-  useEffect(() => {
-    setLoading(true);
-    setError(null);
-    setMapData(null);
-
-    fetch(`https://home-sphere.ca/api/maps/${mapType}`, {
-      headers: {
-        AccessToken: "Kvwf<IQ5qV]nlPooW@",
-      },
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        data.layout = {
-          ...data.layout,
-          margin: { l: 0, r: 0, t: 0, b: 0 },
-          title: "",
-          coloraxis: {
-            ...data.layout.coloraxis,
-            colorbar: {
-              ...data.layout.coloraxis.colorbar,
-              xanchor: "right",
-              yanchor: "middle",
-
-              thickness: 10,
-              x: 0.99,
-              y: 0.5,
-              len: 0.8,
-              width: 0.1,
-              title: {
-                ...data.layout.coloraxis.colorbar.title,
-                side: "right",
-                font: {
-                  family: "Arial Black",
-                  size: 14,
-                },
-              },
-            },
-            colorscale: "Rainbow",
-          },
-          mapbox: {
-            ...data.layout.mapbox,
-            zoom: 11,
-          },
-        };
-        setMapData(data);
-        console.log(data);
-      })
-      .catch((err) => {
-        console.error("Error fetching data:", err);
-        setError(err.message);
-        setMapData(null);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }, [mapType]);
-
   return (
-    <div>
-      <Sidebar></Sidebar>
-
-      {loading && <div className="screen-message">Loading...</div>}
-      {error && (
-        <div className="screen-message">
-          <p>{error}</p>
-        </div>
-      )}
+    <React.Fragment>
       <div
-        id="featureMap"
-        style={{
-          height: "100vh",
-        }}
+        id="sidebar"
+        className={`sidebar ${!lastButtonExpanded ? "shortened" : ""} ${
+          collapseLeft ? "hidden" : ""
+        }`}
       >
-        {mapData && (
-          <Plot
-            data={mapData.data}
-            layout={mapData.layout}
-            useResizeHandler={true}
-            style={{ width: "100%", height: "99%" }}
-            responsive={true}
-          />
-        )}
-      </div>
-    </div>
-  );
-}
+        <div className="HomeSphere-Title">
+          <h1>HOMESPHERE</h1>
+        </div>
+        <div id="buttons-container">
+          <Link to="/maps/congestion">
+            <button
+              id="population-button"
+              className={`menu-button ${
+                isSelected("congestion") ? "selected" : "map-feature-button"
+              }`}
+            >
+              <FontAwesomeIcon
+                icon={faArrowUpRightDots}
+                title="Community Population"
+                className="fa-svg-icon"
+              />
+            </button>
+          </Link>
+          <Link to="/maps/vacancy_per_community">
+            <button
+              id="vacancy-button"
+              className={`menu-button ${
+                isSelected("vacancy_per_community")
+                  ? "selected"
+                  : "map-feature-button"
+              }`}
+            >
+              <FontAwesomeIcon
+                icon={faMapMarkerAlt}
+                title="Land Vacancy"
+                className="fa-svg-icon"
+              />
+            </button>
+          </Link>
+          <Link to="/maps/housing_development_zone">
+            <button
+              id="permits-button"
+              className={`menu-button ${
+                isSelected("housing_development_zone")
+                  ? "selected"
+                  : "map-feature-button"
+              }`}
+            >
+              <FontAwesomeIcon
+                icon={faScroll}
+                title="Building Permits"
+                className="fa-svg-icon"
+              />
+            </button>
+          </Link>
+          <Link to="/maps/property_value_per_community">
+            <button
+              id="pricing-button"
+              className={`menu-button ${
+                isSelected("property_value_per_community")
+                  ? "selected"
+                  : "map-feature-button"
+              }`}
+            >
+              <FontAwesomeIcon
+                icon={faDollarSign}
+                title="House Prices"
+                className="fa-svg-icon"
+              />
+            </button>
+          </Link>
+          <button
+            id="machine-learning-button"
+            className={`menu-button ${
+              isSelected("house_price_map") ? "selected" : "map-feature-button"
+            }`}
+            onClick={() => {
+              toggleMLWindow();
+              handleLastButtonToggle();
+            }}
+          >
+            <FontAwesomeIcon
+              icon={faSquarePollVertical}
+              title="Machine Learning Housing Analysis"
+              className="fa-svg-icon"
+            />
+          </button>
+        </div>
+        <div
+          id="machine-learning-window"
+          className={`${showMLWindow ? "machine-learning-window " : "hidden"}`}
+        >
+          <form>
+            <p>
+              <input
+                type="radio"
+                id="community-level-option"
+                name="ml-options"
+                value="community-level"
+                checked={selectedMLOption === "community-level"}
+                onChange={handleMLOptionChange}
+                className="radio"
+              />
+              <label htmlFor="community-level-option">Community Level</label>
+            </p>
 
-export default Maps;
+            <p>
+              <input
+                type="radio"
+                id="postal-code-level-option"
+                name="ml-options"
+                value="postal-code-level"
+                checked={selectedMLOption === "postal-code-level"}
+                onChange={handleMLOptionChange}
+                className="radio"
+              />
+              <label htmlFor="postal-code-level-option">
+                Sub-Community Level
+              </label>
+            </p>
+          </form>
+
+          <div id="slider" className="slider">
+            <h3>Number of Clusters (Categories):</h3>
+            <Box sx={{}}>
+              <Slider
+                defaultValue={3}
+                getAriaValueText={valuetext}
+                valueLabelDisplay="auto"
+                shiftStep={1}
+                step={1}
+                min={1}
+                max={20}
+                className="slider-component"
+                onChange={clusterCount}
+              />
+            </Box>
+          </div>
+          <div>{getFeatures()}</div>
+          <div id="ml-run-btn" className="ml-run-btn">
+            <button onClick={runML}>Run</button>
+          </div>
+          {/* <div>
+            <button onClick={printResults}>Print Results</button>
+          </div> */}
+        </div>
+      </div>
+      <div className={showMLWindow ? "collapse-expand-left-btn" : "hidden"}>
+        <button
+          onClick={() => setCollapseLeft(!collapseLeft)}
+          className={`collapse-expand-left-btn ${
+            collapseLeft ? "collapse-expand-right-btn" : ""
+          }`}
+        >
+          <img
+            src="https://maps.gstatic.com/tactile/pane/arrow_left_2x.png"
+            className={`sidebar-btn ${collapseLeft ? "right-btn" : ""}`}
+            alt="Toggle Sidebar"
+          />
+        </button>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Sidebar;
